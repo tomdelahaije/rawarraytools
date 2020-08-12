48141b636325057efe6a0cbaeacfb5bc1d784ca4
Tom <tom@di.ku.dk>
2020-08-12 12:01:43 +0200

    Updated description

--- RawArrayTools/PacletInfo.m
+++ RawArrayTools/PacletInfo.m
@@ -1,12 +1,12 @@
 (* Paclet Info File *)
 
-(* created 2017/07/11*)
+(* created 2020/08/12*)
 
 Paclet[
     Name -> "RawArrayTools",
     Version -> "0.1.0",
-    MathematicaVersion -> "11+",
-    Description -> "RawArrayTools` adds a small number of functions to simplify working with RawArray objects. This package is still in development and may be unstable or produce unexpected results.",
+    MathematicaVersion -> "12+",
+    Description -> "RawArrayTools adds a small number of functions to simplify working with RawArray objects.",
     Creator -> "Tom Dela Haije",
     Extensions -> 
         {
